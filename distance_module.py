6b26e612cceaf8dced5bdda0379e112907c87c6b
Craig Handley <craig-handley@hotmail.co.uk>
2016-07-16 19:38:03 +0100

    Added: - Support for requests library Changed: - Small refactoring and code tidy

--- distance_module.py
+++ distance_module.py
@@ -1,5 +1,15 @@
+# Script Name	: distance_module.py
+# Author		: Curtis Handley
+# Created		: 16 Jul 2016
+# Version		: 1.0
+
+# Description	: Measure the distance between a the Ultrasonic emitter and an object.
+
 import RPi.GPIO as GPIO
 import time
+
+# Being module
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
@@ -7,8 +17,8 @@ ECHO = 24
 
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
-
 GPIO.output(TRIG, False)
+
 time.sleep(2)
 
 GPIO.output(TRIG, True)
@@ -24,9 +34,10 @@ while GPIO.input(ECHO)==1:
 pulse_duration = pulse_end - pulse_start
 
 distance = pulse_duration * 17150
-
 distance = round(distance, 2)
 
 print("Distance:", distance, "cm")
 
 GPIO.cleanup()
+
+# End 
